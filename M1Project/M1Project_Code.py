d8323b8134e4c2d791215c65566d89f860131b56
Grayson Myers <graysondmy@gmail.com>
2021-02-08 19:35:57 -0800

    Added Concurrency to start workers for faster image processing

--- M1Project/M1Project_Code.py
+++ M1Project/M1Project_Code.py
@@ -1,7 +1,8 @@
 import os
 import sys
+
 from PIL import Image
-import concurrent
+from concurrent import futures
 
 #Tuple of format types to check if file img format. Note: .endswith works with tuple.
 sup_img_type = (".jpg", ".png")
@@ -12,6 +13,7 @@ file_add_info = "_flipped_resized"
 #Function to go through a directory, change format, size, and rotation of image.
 def correct_images(directory, img_format, size, rotation):
     print(directory)
+    executor = futures.ThreadPoolExecutor()
     if os.path.exists(directory):
         for input_file in os.listdir(directory):
             if input_file.endswith(sup_img_type):
@@ -21,7 +23,7 @@ def correct_images(directory, img_format, size, rotation):
                 try:
                     with Image.open(input_file) as im:
                         print("Correcting: " + input_file + " | Renaming to: " + output_file)
-                        im.rotate(rotation).resize(size).save(output_file)
+                        executor.submit(im.rotate(rotation).resize(size).save(output_file))
                 except OSError as e:
                     print("Error Correcting: " + input_file)
                     print(e)
@@ -29,6 +31,8 @@ def correct_images(directory, img_format, size, rotation):
                 continue
     else:
         print("Not a valid Path: {}".format(directory))
+    print("Waiting for all workers to finish.")
+    executor.shutdown()
 
 # Ask for inputs to make it a bit easier on the user.
 if __name__ == "__main__":
@@ -39,4 +43,3 @@ if __name__ == "__main__":
         size = tuple(map(int,input("What is the size for the images? Ex:1920,1080: ").split(','))),
         rotation = int(input("Specify the rotation of the images? Ex:90: "))
     )
-    
