8ce442b92ae90ccec8f8b0f269e795c91a38c208
Grayson Myers <graysondmy@gmail.com>
2021-02-08 18:52:48 -0800

    Example Text on Input + file name add info variable

--- M1Project/M1Project_Code.py
+++ M1Project/M1Project_Code.py
@@ -3,19 +3,21 @@ import sys
 from PIL import Image
 import concurrent
 
-#Tuple of format types to check if file img format. Also.endswith works with tuple.
+#Tuple of format types to check if file img format. Note: .endswith works with tuple.
 sup_img_type = (".jpg", ".png")
+
+#Text being added to filename.
+file_add_info = "_flipped_resized"
 
 #Function to go through a directory, change format, size, and rotation of image.
 def correct_images(directory, img_format, size, rotation):
-    # directory = os.path.dirname(__file__)
     print(directory)
     if os.path.exists(directory):
         for input_file in os.listdir(directory):
             if input_file.endswith(sup_img_type):
                 print(input_file)
                 f, e = os.path.splitext(input_file)
-                output_file = f + "_flipped_resized" + img_format
+                output_file = f + file_add_info + img_format
                 try:
                     with Image.open(input_file) as im:
                         print("Correcting: " + input_file + " | Renaming to: " + output_file)
@@ -28,11 +30,13 @@ def correct_images(directory, img_format, size, rotation):
     else:
         print("Not a valid Path: {}".format(directory))
 
-# Ask for inputs in case someone hasn't used the file before.
+# Ask for inputs to make it a bit easier on the user.
 if __name__ == "__main__":
     correct_images(
+        #Script execution location is being used as the directory.
         directory = os.getcwd(),
-        img_format = input("What is the specified format: "),
-        size = tuple(map(int,input("What is the size for the images: ").split(','))),
-        rotation = int(input("Specify the rotation of the images: "))
+        img_format = input("What is the specified format? Ex:.jpg: "),
+        size = tuple(map(int,input("What is the size for the images? Ex:1920,1080: ").split(','))),
+        rotation = int(input("Specify the rotation of the images? Ex:90: "))
     )
+    
